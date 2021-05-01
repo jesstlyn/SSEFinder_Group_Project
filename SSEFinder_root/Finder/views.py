78f490d4c4d209327487c2200da394d3cf88760c
Jesstlyn Clarissa <jesstlyn@connect.hku.hk>
2021-05-01 22:32:26 +0800

    add newcaseview file

--- SSEFinder_root/Finder/views.py
+++ SSEFinder_root/Finder/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, View
+from django.contrib import messages
 
-# Create your views here.
+
+
+
+
+
+class AddNewCase(TemplateView):
+    form_class = AddNewClassForm
+    template_name = "addNewCase.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.form_class})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_case = form.save()
+            return redirect("/")
+        else:
+            return render(request, self.template_name, {'form': form})
+
+
+class AddNewCaseForm(forms.ModelForm):
+    template_name = "addNewCase.html"
+
+    class Meta:
+        model = Case
+        fields = ['caseNumber', 'personName', 'identityDocumentNumber', 'birthDate', 'symptomsOnsetDate', 'infectionConfirmationDate']
+
+    def clean_code(self):
+        return self.cleaned_data['caseNumber'].upper()
