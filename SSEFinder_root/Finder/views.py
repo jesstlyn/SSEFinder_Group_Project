9f9a905b435c489a556537fd46d6b29ae2f19628
edward21 <edward210400@gmail.com>
2021-05-02 00:28:26 +0800

    add search page and case number info

--- SSEFinder_root/Finder/views.py
+++ SSEFinder_root/Finder/views.py
@@ -63,4 +63,29 @@ class Login(TemplateView):
                 return render(request,self.template_name,{'form':form})
         else:
             return render(request,self.template_name,{'form':form})
+
+class AddNewCaseForm(forms.ModelForm):
+    template_name = "addNewCase.html"
+
+    class Meta:
+        model = Case
+        fields = ['caseNumber', 'personName', 'identityDocumentNumber', 'birthDate', 'symptomsOnsetDate', 'infectionConfirmationDate']
+
+    def clean_code(self):
+        return self.cleaned_data['caseNumber'].upper()
+            
     
+class AddNewCase(TemplateView):
+    form_class = AddNewClassForm
+    template_name = "addNewCase.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.form_class})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_case = form.save()
+            return redirect("/")
+        else:
+            return render(request, self.template_name, {'form': form})
