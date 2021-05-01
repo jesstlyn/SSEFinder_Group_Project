09a02f89fc90ed79def41112c2e427586c3245c1
edward21 <edward210400@gmail.com>
2021-05-02 00:24:05 +0800

    add search page and case number info

--- SSEFinder_root/Finder/urls.py
+++ SSEFinder_root/Finder/urls.py
@@ -3,5 +3,7 @@ from Finder import views
 
 urlpatterns = [
     path('homePage/', views.homePage.as_view(), name='home-page'),
-    path('login/', views.LoginPage.as_view(), name='login-page')
+    path('login/', views.Login.as_view(), name='login-page'),
+    path('searchCaseNumber/', views.searchCaseNumber.as_view(), name='searchCaseNumber'),
+    path('caseNumberDetail/', views.caseNumberDetail.as_view(), name='caseNumberDetail')
     ]
--- SSEFinder_root/Finder/views.py
+++ SSEFinder_root/Finder/views.py
@@ -1,13 +1,41 @@
-
 from django import forms
 from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView, View
-from Finder.models import Member
+from Finder.models import Member, Case
 from django.contrib import messages
 
 class homePage(TemplateView):
     template_name = 'homePage.html'
+
+class searchCaseNumber(TemplateView):
+    template_name = 'searchCaseNumber.html'
+
+class caseNumberDetail(TemplateView):
+    model = Case
+    template_name = 'caseNumberDetail.html'
+    def get_context_data(self, **kwargs):
+        query = self.request.GET.get('case_number')
+        context = super().get_context_data(**kwargs)
+
+        try :
+            caseInfo = Case.objects.get(caseNumber = query)
+        except:
+            caseInfo = ''
+
+        if (caseInfo == ''):
+            context['message'] = "Data Not Found. Please select another location!"
+        else:
+            caseInfo = Case.objects.get(caseNumber = query)
+            context['message'] = "Data Not Found. Please select another location!"
+            context['caseNumber'] ="Showing details of case number " + caseInfo.caseNumber
+            context['personName'] ="Name: " + caseInfo.personName
+            context['identityDocumentNumber'] ="ID Number: " + caseInfo.identityDocumentNumber
+            context['birthDate'] ="Birth Date: " + caseInfo.birthDate
+            context['symptomsOnsetDate'] ="Symptoms Onset Date: " + caseInfo.symptomsOnsetDate
+            context['infectionConfirmationDate'] ="Infection Confirmation Date: " + caseInfo.infectionConfirmationDate
+
+        return context
 
 
 class LoginForm(forms.ModelForm):
@@ -35,32 +63,4 @@ class Login(TemplateView):
                 return render(request,self.template_name,{'form':form})
         else:
             return render(request,self.template_name,{'form':form})
-
-
-
-class AddNewCaseForm(forms.ModelForm):
-    template_name = "addNewCase.html"
-
-    class Meta:
-        model = Case
-        fields = ['caseNumber', 'personName', 'identityDocumentNumber', 'birthDate', 'symptomsOnsetDate', 'infectionConfirmationDate']
-
-    def clean_code(self):
-        return self.cleaned_data['caseNumber'].upper()
-            
     
-class AddNewCase(TemplateView):
-    form_class = AddNewClassForm
-    template_name = "addNewCase.html"
-
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            new_case = form.save()
-            return redirect("/")
-        else:
-            return render(request, self.template_name, {'form': form})
- 
